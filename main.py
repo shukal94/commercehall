dd3c0797d8672f2022536505f6a125d17653f419
Sergei <sergeitrigubovfleps@gmail.com>
2019-04-11 11:20:13 +0300

    improved functionality
    removed syntax errors
    add resize function
    zoom feature added

--- main.py
+++ main.py
@@ -3,30 +3,65 @@ from PIL import Image, ImageDraw
 
 
 app = Flask(__name__)
+zoom = 3
 
 
 @app.route("/", methods=['GET', 'POST'])
-def make_matrix():
-    a = request.json
-    dict = {'Not_Available': 'red', 'Available': 'green', 'Cashbox': 'yellow', 'Rack': 'blue'}
-    img = Image.new('RGB', (1400, 1000), color='white')
+def draw_scheme():
+    state_matrix = json.request
+    state_dictionary = {'Not_Available': 'red',
+                        'Available': 'green',
+                        'Cashbox': 'yellow',
+                        'Rack': 'blue',
+                        'Done': 'black'}
+    draw_image(state_matrix, state_dictionary)
+    return "Done"
+
+
+def draw_image(state_matrix, state_dictionary):
+    img = Image.new('RGB', (700*zoom, 500*zoom), color='white')
     draw = ImageDraw.Draw(img)
-    pointer_y = 0
-    zoom = 2
-    for point_rows in a:
-        pointer_x = 0
+    point_y = 0
+    for point_rows in state_matrix:
+        point_x = 0
         for point_columns in point_rows:
-            draw.rectangle(
-                (
-                    (pointer_x)*zoom,
-                    (pointer_y)*zoom,
-                    (pointer_x+1)*zoom,
-                    (pointer_y+1)*zoom
-                ),
-                fill=dict[point_columns]
-            )
-    img.save('Схема.png')
-    return json.jsonify(size)
+            if point_columns != 'Done':
+                if point_columns == 'Available':
+                    draw.rectangle(
+                        (
+                            point_y,
+                            point_x,
+                            (point_y + 1),
+                            (point_x + 1)
+                        ),
+                        fill=state_dictionary[point_columns]
+                    )
+                else:
+                    temp_img = Image.open(point_columns + '.jpg')
+                    new_size = calculate_new_size(point_x,
+                                                  point_y,
+                                                  state_dictionary,
+                                                  state_matrix)
+                    temp_img = temp_img.resize((new_size[1], new_size[0]), Image.ANTIALIAS)
+                    img.paste(temp_img, (point_y, point_x))
+            point_x += 1
+        point_y += 1
+    img.save('Scheme.png')
+
+
+def calculate_new_size(point_x, point_y, state_dictionary, state_matrix):
+    furniture = state_dictionary[state_matrix[point_y][point_x]]
+    temp_point_y = point_y
+    temp_point_x = point_x
+    detect_size = True
+    while detect_size:
+        if state_dictionary[state_matrix[temp_point_y][point_x]] != furniture:
+            return [temp_point_x - point_x, temp_point_y - point_y]
+        temp_point_x = point_x
+        while state_dictionary[state_matrix[temp_point_y][temp_point_x]] == furniture:
+            state_matrix[temp_point_y][temp_point_x] = 'Done'
+            temp_point_x += 1
+        temp_point_y += 1
 
 
 if __name__ == '__main__':
